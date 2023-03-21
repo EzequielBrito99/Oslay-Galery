faa7c8564fd0557235b5afd72647b7e914dbdac4
Ezequiel <ebritonavarro@gmail.com>
2023-03-21 18:58:23 -0400

    Corregido scroll de la flecha en el header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { animateScroll } from 'react-scroll';
 
 const Header = () => {
   const ancho = window.screen.width;
@@ -13,13 +14,17 @@ const Header = () => {
     }
   }, [ancho]);
 
+  const handleArrowDown = () => {
+    animateScroll.scrollTo(window.screen.height, {smooth:true, duration:1500});
+  };
+
   return (
     <header className='header-wrapper d-flex flex-column justify-content-center align-items-center' 
             style={{backgroundImage: fondo}}
     >
-      <h1>Oslay Moya</h1>
-      <p>Artista plástico</p>
-      <a href="#contenido"><i className="fa fa-angle-double-down mt-5" aria-hidden="true"></i></a>
+      <h1 className='animate__animated animate__fadeIn'>Oslay Moya</h1>
+      <p className='animate__animated animate__fadeIn'>Artista plástico</p>
+      <a className='animate__animated animate__fadeIn' onClick={handleArrowDown}><i className="fa fa-angle-double-down mt-5" aria-hidden="true"></i></a>
     </header>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
   );
 };
